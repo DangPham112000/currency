5df104824baf58a4976856cd05b1b801ff4e8d0b
DangPham <dangpham112000@users.noreply.github.com>
2021-10-19 21:23:21 +0700

    Add project files.

---
+++ Currency/Controllers/CurrencyController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Currency.Controllers
+{
+    public class CurrencyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("myview");
+        }
+
+        public IActionResult transfer(float dollar, float rate)
+        {
+            Models.Currency cur = new Models.Currency(dollar, rate);
+            cur.transfer();
+            return View("myview", cur);
+        }
+    }
+}
---
+++ Currency/Models/Currency.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Currency.Models
+{
+    public class Currency
+    {
+        public float dollar;
+        public float rate;
+        public object vnd;
+
+        public Currency(float dollar, float rate)
+        {
+            this.dollar = dollar;
+            this.rate = rate;
+        }
+        
+        public void transfer()
+        {
+            vnd = rate * dollar;
+        }
+    }
+}
